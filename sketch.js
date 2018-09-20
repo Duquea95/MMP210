8adc54a5c08447f1d50321f6df0268895b3a0723
Anthony Duque <duquea95@gmail.com>
2018-09-20 14:18:10 -0400

    color change

--- sketch.js
+++ sketch.js
@@ -1,30 +1,30 @@
 function setup() {
-    createCanvas(600,600);
+    createCanvas(800,800);
 }
 
 function draw(){
 
     var bg = 200;
-    var headX = 200, headY = 200, headW = 200, headH = headW+50;
+    var headX = 300, headY = 300, headW = 300, headH = headW+50;
 
     var hairX = 300, hairY = 150, hairW = 240, hairH = 150, hairStart = 700, hairClose = 120, hairType = OPEN;
 
     var eyesRX = 350, eyesRY = 280, eyesRW = 50, eyesRH = 25, pupilRH = 50;
     var eyesLX = 250, eyesLY = 280, eyesLW = 50, eyesLH = 25, pupilLH = 50;
 
-    var earLX = 175, earLY = 250, earLW = 40, earLH = 40;
-    var earRX = 425, earRY = 250, earRW = 40, earRH = 40;
+    var earLX = 140, earLY = 250, earLW = 40, earLH = 40;
+    var earRX = 460, earRY = 250, earRW = 40, earRH = 40;
 
     var mouthX = 250, mouthY = 380, mouthW = 350, mouthH = 380;
 
-    var earColor = "yellow", eyeColor = "white", pupilColor = "black", hairColor = "black", headColor = "yellow";
+    var earColor = "rgb(214,185,140)", eyeColor = "white", pupilColor = "black", hairColor = "black", headColor = "rgb(214,185,140)";
 
 
     background(bg);
 
     // head
     fill(headColor);
-    rect(headX,headY,headW,headH);
+    ellipse(headX,headY,headW,headH);
 
     // hair
     fill(hairColor);
