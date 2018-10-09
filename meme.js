d67832f8593e2f3f835e6444c91a6b332ec30031
Anthony Duque <duquea95@gmail.com>
2018-10-09 14:37:24 -0400

    add more text

--- meme.js
+++ meme.js
@@ -15,7 +15,16 @@ function draw(){
     fill('plum');
     textFont('Comic Sans MS');
     text("Much Animation", 20,100);
-    textSize(60);
+    textSize(40);
+
+    text("Wow P5", 520,200);
+    textSize(40);
+
+    text("Such Usefulness", 20,600);
+    textSize(40);
+
+    text("Wow", 520,700);
+    textSize(40);
 
 	image(img,150, 150, mouseX, mouseY);
     tint(255, 0, 255);
