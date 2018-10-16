70505cc7a1b48825821df80cb5613b9929129d91
Anthony Duque <duquea95@gmail.com>
2018-10-16 14:19:14 -0400

    edits

--- conditional.js
+++ conditional.js
@@ -10,6 +10,7 @@ function setup() {
 }
 
 function draw(){
+    rectMode(CENTER);
     // fill('plum');
     // textFont('Comic Sans MS');
     // text("Much Animation", 20,100);
@@ -24,40 +25,45 @@ function draw(){
     // text("Wow", 520,640);
     // textSize(40);
     //
-    background('white');
+    background('black');
 	// image(img,150,150, mouseX, mouseY);
     // tint(255, 0, 255);
-    strokeWeight(1); stroke("black");
+    strokeWeight(1); stroke("white");
     line(width/2, 0, width/2, height);
     line(0, height/2, width, height/2);
     // fill('black');
 
     if (mouseX < width/2 && mouseY < height/2) {
         // top left
-        image(img,50,50, mouseX, mouseY);
+        image(img, mouseX, mouseY, 250, 250);
         tint(255, 0, 255);
         textFont('Comic Sans MS');
         text("Much Animation", 20,100);
         textSize(40);
         fill('plum');
+        // if (mouseIsPressed) {
+        //     var s = (mouseX - pmouseX) + (mouseY - pmouseY);
+        //     ellipse(mouseX, mouseY, s);
+        // }
+
     } else if (mouseX > width/2 && mouseY < height/2) {
         // top right
-        image(img,50,50, mouseX, mouseY);
+        image(img, mouseX, mouseY, 250, 250);
         tint('green');
         text("Wow P5", 520,200);
         textSize(40);
         fill('lightblue');
     } else if (mouseX < width/2 && mouseY > height/2) {
         // bottom left
-        image(img,50,50, mouseX, mouseY);
+        image(img, mouseX, mouseY, 250, 250);
         tint('gold');
         text("Such Usefulness", 20,580);
         textSize(40);
         fill('red')
     } else {
         // bottom right
-        image(img,50,50, mouseX, mouseY);
-        tint('lightgreen');
+        image(img, mouseX, mouseY, 250, 250);
+        tint('white');
         text("Wow", 520,640);
         textSize(40);
         fill('yellow');
