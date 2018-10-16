11beaa3d38ee551fb50ba00c2f56b8c0780cd29f
Anthony Duque <duquea95@gmail.com>
2018-10-16 14:22:39 -0400

    update

--- conditional.js
+++ conditional.js
@@ -41,18 +41,22 @@ function draw(){
         text("Much Animation", 20,100);
         textSize(40);
         fill('plum');
-        // if (mouseIsPressed) {
-        //     var s = (mouseX - pmouseX) + (mouseY - pmouseY);
-        //     ellipse(mouseX, mouseY, s);
-        // }
+        if (mouseIsPressed) {
+            fill('blue');
+        }
 
     } else if (mouseX > width/2 && mouseY < height/2) {
         // top right
         image(img, mouseX, mouseY, 250, 250);
         tint('green');
-        text("Wow P5", 520,200);
+        var text1 = text("Wow P5", 520,200);
         textSize(40);
         fill('lightblue');
+        if (mouseIsPressed) {
+            // text1.remove();
+            // text("I'm a floating head", 520,200);
+            fill('white');
+        }
     } else if (mouseX < width/2 && mouseY > height/2) {
         // bottom left
         image(img, mouseX, mouseY, 250, 250);
@@ -60,6 +64,11 @@ function draw(){
         text("Such Usefulness", 20,580);
         textSize(40);
         fill('red')
+        if (mouseIsPressed) {
+            // text1.remove();
+            // text("I'm a floating head", 520,200);
+            fill('purple');
+        }
     } else {
         // bottom right
         image(img, mouseX, mouseY, 250, 250);
@@ -67,6 +76,11 @@ function draw(){
         text("Wow", 520,640);
         textSize(40);
         fill('yellow');
+        if (mouseIsPressed) {
+            // text1.remove();
+            // text("I'm a floating head", 520,200);
+            fill('green');
+        }
     }
 
     // if (mouseIsPressed) {
